25a2e8e776f144f7f5c12e770426836f0e6f811b
tamanghawana0107 <hawanatamang@gmail.com>
2022-03-15 14:44:33 +0545

    added

---
+++ HW questions.cpp
@@ -0,0 +1,7 @@
+//WAP to add two times in hour and minute illustrating passing object as function arguments.
+//WAP to calculate the area of rectangle of using default arguments.
+//WAP to overload a function named power which calculates a^b or (a+b)^c if two or three arguments are passed respectively.
+//Using inline function,WAP to add 2 numbers and return sum.
+//WAP to overload a function named func_area(),which calculates and returns area of circle and rectangle.Assume appropriate number 
+//and type of arguments and return type.
+//WAP to find perimeter of a square ,rectangle and circle using function overloading.
